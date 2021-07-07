1d21a26beb0f7a8d761a1be5a0d1fc79b7402c4e
Liana Baryshova <barishovaliana@gmail.com>
2021-07-08 00:12:15 +0200

    Add files

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -8,25 +8,17 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
+const toggleUserState = (allUsers, userName) => {
   const updatedUsers = allUsers.map(user =>
     user.name === userName ? { ...user, active: !user.active } : user,
   );
 
-  callback(updatedUsers);
+  return new Promise((resolve, reject) => {
+    resolve(updatedUsers);
+  }); 
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
-
-/*
- * Должно работать так
-
- */
-// toggleUserState(users, 'Mango').then(logger);
-// toggleUserState(users, 'Lux').then(logger);
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
